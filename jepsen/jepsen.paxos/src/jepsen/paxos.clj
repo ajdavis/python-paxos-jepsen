7634f1a8af1f0892de7b4f9d64740d47db1b90cf
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2021-12-04 11:44:51 -0500

    jepsen can tear down paxos servers?

--- jepsen/jepsen.paxos/src/jepsen/paxos.clj
+++ jepsen/jepsen.paxos/src/jepsen/paxos.clj
@@ -24,7 +24,9 @@
   "Rsync source files to destination. (Because Jepsen's upload doesn't support recursive, I think.)"
   [& args]
   (apply call-shell
-         (concat '("rsync" "-e" "/usr/bin/ssh '-o StrictHostKeyChecking=no'" "-az") args)))
+         (concat
+          '("rsync" "-e" "/usr/bin/ssh '-o StrictHostKeyChecking=no'" "-az")
+          args)))
 
 (defn db
   "Not a DB, a Paxos node."
@@ -34,19 +36,24 @@
    (setup! [_ test node]
            (info "installing Python-Paxos, rsyncing code and config")
            ; Upload this code and config file to worker node.
-           (rsync "/home/admin/python-paxos-jepsen/" (str node ":python-paxos-jepsen"))
+           (rsync "/home/admin/python-paxos-jepsen/"
+                  (str node ":python-paxos-jepsen"))
            (rsync "../../../nodes" (str node ":"))
            ; Upload Python 3.9. See README for building Python.
            ; TODO: README instructions
            (rsync "/home/admin/python3.9" (str node ":"))
            ; Executed on the remote worker node.
            (info "pip-installing requirements")
-           (c/exec "/home/admin/python3.9/bin/pip3" "install" "-r" "/home/admin/python-paxos-jepsen/paxos/requirements.txt")
+           (c/exec "/home/admin/python3.9/bin/pip3" "install" "-r"
+                   "/home/admin/python-paxos-jepsen/paxos/requirements.txt")
            (info "starting daemon")
            (c/su
             (c/exec "/bin/bash" "/home/admin/python-paxos-jepsen/start-daemon.sh")))
    (teardown! [_ test node]
-              (info node "tearing down Paxos (TODO)"))))
+              (info node "tearing down Paxos")
+              (c/su
+               (c/exec "/sbin/start-stop-daemon" "--stop" "--pidfile" "/var/paxos.pid" "--exec"
+                       "/home/admin/python3.9/bin/python3.9" "--oknodo")))))
 
 (defn paxos-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
